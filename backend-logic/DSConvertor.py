0c09b72af51de6c84b5450a718ec722575e86b45
Daniel <razyback06@gmail.com>
2025-06-16 18:32:53 -0500

    Good Basis For DSLog to Csv Convertor

--- backend-logic/DSConvertor.py
+++ backend-logic/DSConvertor.py
@@ -1,18 +1,35 @@
 import dslogparser as dslp
+from pathlib import Path
+import pathlib
 import os
 
 class DSConvertor:
     def __init__(self,dsLogDir=""):
         self.dsLogDir=dsLogDir
-        self.destinationDr=""
-        self.exclusionListFP=""
+        self.destinationDr=os.path.dirname(os.path.realpath(__file__))+"/"+"csvDSLogs"
+        self.exclusionListFP=os.path.dirname(os.path.realpath(__file__))+"/"+"exclusionListFP.txt"
 
     def processDSLogs(self):
-        for file in os.listdir(self.dsLogDir):
-            fp=self.dsLogDir+"/"+file
-            newDSLP=dslp.DSLogParser(fp)
-            csvData=newDSLP.parse_data_v3()
+        for file in os.listdir(self.dsLogDir)[0:2]:
+            with Path.open(self.exclusionListFP) as exF:
+                if file[file.__len__()-5:file.__len__()] == "dslog" and file not in exF.read():
+                    fp=self.dsLogDir+"/"+file
+                    newDSLP=dslp.DSLogParser(fp)
+                    newDSLP.read_records()
+                    v=newDSLP.read_record_v3()
+                    self.addToExclusionList(file)
 
     def addToExclusionList(self,fileName=""):
-        with os.open() as f:
-            f
+        if Path(self.exclusionListFP).is_file:
+            open("exclusionListFP.txt", "a")
+        else:
+            open("exclusionListFP.txt", "x")
+
+        with Path.open(self.exclusionListFP,"a") as f:
+            f.write(fileName+"\n")
+
+dslogdir=r"C://Users/Public\Documents/FRC/Log Files\DSLogs"
+
+dsconv=DSConvertor(dslogdir)
+
+dsconv.processDSLogs()
