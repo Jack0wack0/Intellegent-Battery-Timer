6ea7660a0cdc64b3b529ad03e84091745921873a
Daniel <razyback06@gmail.com>
2025-06-16 19:06:00 -0500

    Auto Generation of CSV Files

--- backend-logic/DSConvertor.py
+++ backend-logic/DSConvertor.py
@@ -6,8 +6,8 @@ import os
 class DSConvertor:
     def __init__(self,dsLogDir=""):
         self.dsLogDir=dsLogDir
-        self.destinationDr=os.path.dirname(os.path.realpath(__file__))+"/"+"csvDSLogs"
-        self.exclusionListFP=os.path.dirname(os.path.realpath(__file__))+"/"+"exclusionListFP.txt"
+        self.destinationDr=os.path.join(os.path.dirname(os.path.realpath(__file__)),"csvDSLogs")
+        self.exclusionListFP=os.path.join(os.path.dirname(os.path.realpath(__file__)),"exclusionListFP.txt")
 
     def processDSLogs(self):
         for file in os.listdir(self.dsLogDir)[0:2]:
@@ -17,13 +17,14 @@ class DSConvertor:
                     newDSLP=dslp.DSLogParser(fp)
                     newDSLP.read_records()
                     v=newDSLP.read_record_v3()
+                    Path.open(os.path.join(self.destinationDr,file[0:file.__len__()-6]+".csv"), "x")
                     self.addToExclusionList(file)
 
     def addToExclusionList(self,fileName=""):
         if Path(self.exclusionListFP).is_file:
-            open("exclusionListFP.txt", "a")
+            open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"exclusionListFP.txt"), "a")
         else:
-            open("exclusionListFP.txt", "x")
+            open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"exclusionListFP.txt"), "x")
 
         with Path.open(self.exclusionListFP,"a") as f:
             f.write(fileName+"\n")
