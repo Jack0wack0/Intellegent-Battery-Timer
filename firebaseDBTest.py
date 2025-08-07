453d4e913a50277917ad8957f26fa93de0fb2875
dlong-cw <71839102+dlong-cw@users.noreply.github.com>
2025-08-06 22:02:52 -0500

    Inital DB Tests

---
+++ firebaseDBTest.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FIREBASE_DB_BASE_URL = getenv('FIREBASE_DB_BASE_URL')
+FIREBASE_CREDS_FILE = getenv('FIREBASE_CREDS_FILE')
+
+# Initialize the app with a service account, granting admin privileges
+cred = credentials.Certificate(FIREBASE_CREDS_FILE)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': FIREBASE_DB_BASE_URL
+})
+
+ref = db.reference('/')
+
+ref.child('test').set({
+          'name': 'Helloworld',
+          'id': 5454
+      })
+
+id_test = ref.child('test/id').get()
+
+print('Data from Firebase: ' + str(id_test))
