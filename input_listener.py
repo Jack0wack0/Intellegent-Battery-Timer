cd1f9edcec6dd090338d2b2a15c29e286033e0e2
dlong-cw <71839102+dlong-cw@users.noreply.github.com>
2025-08-08 17:48:32 -0500

    Changes for not updating when under settings change

--- input_listener.py
+++ input_listener.py
@@ -144,7 +144,7 @@ def handle_serial():
 
                     #Get the current (Now removed) charging slot for this battery/tag
                     chargingSlot = ref.child(f'BatteryList/{prev_tag}/ChargingSlot').get()
-                  
+
                     #Pull all records of charging for this battery/tag
                     getCurrentChargingRecords = ref.child('BatteryList/' + prev_tag + '/ChargingRecords').get()
 
@@ -158,7 +158,7 @@ def handle_serial():
 
                     endTime = datetime.strptime(endTime, "%Y-%m-%d %H:%M:%S") #Convert to datetime object
                     startTime = datetime.strptime(startTime, "%Y-%m-%d %H:%M:%S") #Convert to datetime object
-                    duration = endTime - startTime #Determine the duration between start and end time
+                    duration = endTime - startTime #Determine the duration between start and end time 
 
                     #Update the most recent record with the end time and duration, count-1 is used to get the most recent record since arrays are 0 indexed in Firebase
                     ref.child(f'BatteryList/{prev_tag}/ChargingRecords/{count-1}').update({'EndTime': endTimeStamp, 'Duration': str(duration.total_seconds())[:-2]}) #Duration is saved in SECONDS with removing the default '.0' left with the total_seconds method I.E '30.0' seconds is saved as '30'
@@ -190,7 +190,18 @@ def handle_serial():
                       avgDuration = overallDuration/totalCycles   #Average charge time is the overall charge time divided by the number of cycles
                       avgDuration = "{:.0f}".format(avgDuration) #Format to remove decimal places, this also rounds DOWN by removing the decimal places
 
-                    ref.child('BatteryList/' + prev_tag).update({
+                    if int(str(duration.total_seconds())[:-2]) < minTimeSetting:
+                        ref.child('BatteryList/' + prev_tag).update({
+                        'ID': prev_tag,
+                        'IsCharging': False, #Set charging as false
+                        'ChargingSlot': None, #Remove the ChargingSlot as it's no longer charging
+                        'LastChargingSlot': chargingSlot, #Set the last charging slot to the current slot it was charging in
+                        'TotalCycles' : totalCycles, #Total number of charge cycles for this battery/tag
+                        'AverageChargeTime': avgDuration, #Average charge time in seconds
+                        'OverallChargeTime': overallDuration, #Overall lifetime charge time in seconds
+                    })
+                    else:
+                        ref.child('BatteryList/' + prev_tag).update({
                         'ID': prev_tag,
                         'IsCharging': False, #Set charging as false
                         'ChargingSlot': None, #Remove the ChargingSlot as it's no longer charging
