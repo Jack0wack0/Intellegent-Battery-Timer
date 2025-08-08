f48e817c5c7a82410a5c62e5232cc8cd2f4b5c38
dlong-cw <71839102+dlong-cw@users.noreply.github.com>
2025-08-08 14:04:33 -0500

    changes to int

--- input_listener.py
+++ input_listener.py
@@ -179,7 +179,7 @@ def handle_serial():
                     #Note, everything is in SECONDS
                     overallDuration = 0
                     for record in getCurrentChargingRecords:
-                        overallDuration += record.get('Duration') #Overall charge time is the sum of all durations in the records array
+                        overallDuration += int(record.get('Duration')) #Overall charge time is the sum of all durations in the records array
 
                     avgDuration = overallDuration/count   #Average charge time is the overall charge time divided by the number of cycles
                     avgDuration = "{:.0f}".format(avgDuration) #Format to remove decimal places, this also rounds DOWN by removing the decimal places
