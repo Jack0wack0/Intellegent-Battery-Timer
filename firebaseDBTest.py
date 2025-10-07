10133a8cbdb0b79fb64713c664b73a2f4504443b
Jack0wack0 <75100830+Jack0wack0@users.noreply.github.com>
2025-10-06 22:20:54 -0500

    test

--- firebaseDBTest.py
+++ firebaseDBTest.py
@@ -4,6 +4,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from dotenv import load_dotenv
+import time
+from datetime import datetime
 
 load_dotenv()
 
@@ -16,13 +18,22 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': FIREBASE_DB_BASE_URL
 })
 
+def timestamp(ts=None):
+    return datetime.fromtimestamp(ts or time.time()).strftime("%Y-%m-%d %H:%M:%S")
+
+prevtag = "0000000001"
 ref = db.reference('/')
+now = time.time()
 
-ref.child('test').set({
-          'name': 'Helloworld',
-          'id': 5454
-      })
+ref.child('BatteryList/' + prevtag).update({
+                        'ID': prevtag, #Battery Tag ID
+                        'IsCharging': True, #Set charging as true
+                        'ChargingSlot': 0, #Current slot the battery is charging in
+                        'ChargingStartTime': timestamp(now), #When was the most recent time it started charging - used to determine how long it's been charging for/Now time
+                        'ChargingEndTime': None, #Remove the ChargingEndTime as it's currently charging
+                        'LastChargingSlot': None, #Remove the LastChargingSlot as it's currently charging
+})
 
-id_test = ref.child('BatteryList/1').get()
+#id_test = ref.child('BatteryList/1').get()
 
-print('Data from Firebase: ' + str(id_test))
+#print('Data from Firebase: ' + str(id_test))
